96285e0e4653836821dba19cdbdddcbcd4a0a5f7
Emerson Del Orbe Candelario <emerson_delorbe@ucne.edu.do>
2020-07-09 13:59:50 -0400

    Add project files.

---
+++ Parcial2Azure/Models/FAQ.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parcial2Azure.Models
+{
+    class FAQ
+    {
+        public string Id { get; set; }
+        public string pregunta { get; set; }
+        public string respuesta { get; set; }
+    }
+}
---
+++ ParcialAzure2/CovidFAQ.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs;
+using ParcialAzure2.Models;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Threading.Tasks;
+
+
+namespace ParcialAzure2
+{
+    class CovidFAQ
+    {
+        [FunctionName("covidFAQ")]
+        public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        [CosmosDB(
+            databaseName:"Cosmodb",
+            collectionName:"CosmodbConteiner",
+            ConnectionStringSetting = "DBConnectionString"
+            )] IEnumerable<FAQ> questionSet,
+        ILogger log)
+        {
+            log.LogInformation("Data fetched from CosmodbConteiner");
+            return new OkObjectResult(questionSet);
+        }
+    }
+}
